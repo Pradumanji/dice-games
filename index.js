a7c00631260501f2eb49dc2f380b3395eaba929a
Praduman-gupta <103199278+Pradumanji@users.noreply.github.com>
2022-10-23 00:48:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// alert("working man")
+
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
 var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
 var randomImageSource1 = "images/" + randomDiceImage; // images/dice1.png - images/dice6.png
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource1);
-// console.log(randomNumber1);
+
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
